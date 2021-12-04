ce37a4981a2c0c6bf86864a0cb63b12c6ac5f392
dark-red-apple <alma68@gmail.com>
2021-12-04 21:52:53 +0330

    Update left and right navs

--- js.js
+++ js.js
@@ -5,6 +5,7 @@ slideMain.style.height = slides[0].querySelector('img').offsetHeight +'px'
 let curslide = 0
 let prevSlide
 let intervId
+let tiId
 initNavs()
 showSlide()
 
@@ -22,7 +23,7 @@ function goToRight(curslide, prevSlide){
 
     slides[prevSlide].style.transition = "transform 1s linear"
     slides[prevSlide].style.transform = "translate(200%)"
-    setTimeout(function(){
+    tiId = setTimeout(function(){
         slides[prevSlide].style.transition = "unset"
         slides[prevSlide].style.transform = "unset"        
     },1000)
@@ -35,20 +36,16 @@ function goToRight(curslide, prevSlide){
 }
 
 function goToLeft(curslide, prevSlide){
-
+ 
     slides[prevSlide].style.transition = "transform 1s linear"
     slides[prevSlide].style.transform = "translate(0)"
-    setTimeout(function(){
-        slides[prevSlide].style.transition = "unset"
-        slides[prevSlide].style.transform = "translate(200%)"        
-    },1000)
-    
+  
     slides[curslide].style.transition = "transform 1s linear"
     slides[curslide].style.transform = "translate(100%)"
 
-    slideMain.style.height = slides[curslide].querySelector('img').offsetHeight +'px'       
-
+    slideMain.style.height = slides[curslide].querySelector('img').offsetHeight +'px'      
 }
+
 function leftInit(){
     slides.forEach(function(slide){
         if(slides[curslide]!= slide) {            
@@ -57,17 +54,67 @@ function leftInit(){
         }
     })
 }
+function rightInit(){
+    slides.forEach(function(slide){
+        if(slides[curslide]!= slide) {            
+            slide.style.transition = "unset"
+            slide.style.transform = "translate(0)"
+        }
+    })
+}
 
 function initNavs(){
-    slideNav.forEach(function(nav){
-        nav.addEventListener('click',function(){
-            clearInterval(intervId)
+    const leftNav = slideMain.querySelector('.slide-nav.left')
+    const rightNav = slideMain.querySelector('.slide-nav.right')
+    rightNav.addEventListener('click',function(){
+        clearInterval(intervId)
+        
+        if(slides[curslide].getBoundingClientRect().x != 0){
+
+            slides[prevSlide].style.transition = "transform 0.2s linear"
+            slides[curslide].style.transition = "transform 0.2s linear"
+
+        }
+        else{
+            rightInit()
             setCurPrevSlides()
             goToRight(curslide, prevSlide)
+        }
+
+        setTimeout(function(){
+            showSlide()  
+        },1000)            
+    })
+
+    leftNav.addEventListener('click',function(){
+        clearInterval(intervId)
+        clearInterval(tiId)
+
+        if(slides[curslide].getBoundingClientRect().x != 0){
+            let curslideX = 100 + (((slides[curslide].getBoundingClientRect().x)/slideMain.offsetWidth)*100)
+
+            slides[prevSlide].style.transition = "unset"
+            slides[prevSlide].style.transform = "unset"  
+            slides[curslide].style.transform = `translate(${curslideX}%)`
+            slides[prevSlide].style.transform = `translate(${100+curslideX}%)`
+            setCurPrevSlidesToLeft()  
+            goToLeft(curslide, prevSlide)  
+        }
+        else{
+            leftInit()
+            setCurPrevSlidesToLeft()  
+ 
             setTimeout(function(){
-                showSlide()  
-            },1000)            
-        })
+                goToLeft(curslide, prevSlide)  
+    
+            })
+        }      
+
+        setTimeout(function(){
+            rightInit()
+            showSlide()  
+        },1000)
+      
     })
 }
 
@@ -76,6 +123,14 @@ function setCurPrevSlides(){
     curslide++
     if(curslide>slides.length-1){
         curslide = 0      
+    } 
+}
+
+function setCurPrevSlidesToLeft(){
+    prevSlide = curslide
+    curslide--
+    if(curslide<0){
+        curslide = slides.length-1      
     } 
 }
 
